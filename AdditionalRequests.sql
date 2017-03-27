c6055520278444c62cde22be3a3015556cb5a7af
Dmitry <diman.mymail@rambler.ru>
2017-03-27 17:25:17 +0300

    Added requests for adding data to tables "region" and "city"
    Added requests for adding data to tables "region" and "city"

--- AdditionalRequests.sql
+++ AdditionalRequests.sql
@@ -1,4 +1,52 @@
 DELIMITER $$
+DROP PROCEDURE IF EXISTS setStoragesData
+$$
+CREATE PROCEDURE setStoragesData()
+BEGIN 
+	SET @counter = 0;
+	WHILE @counter < 3
+		DO
+			INSERT INTO storages (name)
+			VALUES (CONCAT('sklad',FLOOR(RAND()*1000)));
+			SET @counter = @counter + 1;
+		END WHILE;
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS setRegionData
+$$
+CREATE PROCEDURE setRegionData()
+BEGIN 
+	SET @counter = 0;
+	WHILE @counter < 3
+		DO
+			INSERT INTO region (name)
+			VALUES (CONCAT('region',FLOOR(RAND()*1000)));
+			SET @counter = @counter + 1;
+		END WHILE;
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS setCityData
+$$
+CREATE PROCEDURE setCityData()
+BEGIN 
+	SET @counter = 0;
+	WHILE @counter < 3
+		DO
+			INSERT INTO city (name, region_id)
+			VALUES (CONCAT('city',FLOOR(RAND()*1000)),(SELECT id FROM region WHERE id = (@counter+1))) ;
+			SET @counter = @counter + 1;
+		END WHILE;
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
 DROP PROCEDURE IF EXISTS setBankIncomeData
 $$
 CREATE PROCEDURE setBankIncomeData()
